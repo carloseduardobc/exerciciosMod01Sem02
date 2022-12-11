71c3f7939fa4490a000a62c0f6074cbd0bc7a7a4
carloseduardobc <carlosedubc@gmail.com>
2022-12-11 20:55:22 -0300

    Finalizando o arquivo do exercício 02

---
+++ exercicio02/index.js
@@ -0,0 +1,22 @@
+/*
+Utilizando estrutura condicional exiba a descrição da classificação da idade da pessoa de acordo com os dados abaixo:
+Jovens - Indivíduos de até 15 anos;
+Adultos - Indivíduos com idade entre 16 até 64 anos;
+Idosos - Indivíduos de 65 anos em diante.
+
+Utilize prompt para solicitar a idade e alert para exibir a classificação.
+
+Realize o commit do exercício resolvido.
+*/
+
+var idade = parseInt(prompt('Informe a sua idade: '));
+
+if(idade >= 0 && idade <= 15){
+    alert('Jovens');
+} else if(idade >=16 && idade <= 64){
+    alert('Adultos');
+} else if(idade >= 65){
+    alert('Idosos');
+}else{
+    alert('Valor Inválido');
+}
