32962766833916e21dfb6ce5bbb5013170430922
carloseduardobc <carlosedubc@gmail.com>
2022-12-19 22:58:01 -0300

    Solução do exercício 8 da semana2

---
+++ exercicio08/indexEx08.js
@@ -0,0 +1,22 @@
+/*
+Ao entrar na página, seu script deve realizar 2 prompts ao usuário: um solicitando um “valor inicial”, e outro um valor para a “raíz”.
+Após o usuário inserir esses valores, o seu script deve calcular os 10 primeiros valores da sequência e exibir na tela.
+Relembrando: P.A. (Progressão Aritmética) é uma sequência numérica em que cada termo, a partir do segundo, é igual à soma do termo
+anterior com a raiz. Exemplo: Valor inicial = 1; raiz = 3; P.A. = 1, 4, 7, 10, 13, 16, 19, 22, 25, 28.
+
+Realize o commit do exercício resolvido.
+*/
+
+var valorInicial = parseInt(prompt('Informe o valor inicial: '));
+var raiz = parseInt(prompt('Informe o valor de progressão: '));
+
+var soma = valorInicial;
+var resultado = valorInicial.toString();
+
+for (var i = 0; i < 9; i++){
+    // soma += raiz;
+    // resultado += ', ' + soma;
+    soma = soma + raiz;
+    resultado = resultado + soma;
+}
+alert('A PA é: ' + resultado);
